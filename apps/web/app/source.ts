60ae167b418c1d4a916fdcca9b8834faa7503a3d
Xiangzhuang Shen <datahonor@gmail.com>
2024-07-24 16:40:54 +0800

    polish

--- apps/web/app/source.ts
+++ apps/web/app/source.ts
@@ -1,5 +1,7 @@
 import { map } from '@/.map';
-import { createMDXSource } from 'fumadocs-mdx';
+import { z } from 'zod';
+
+import { createMDXSource, defaultSchemas } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
 
 export const { getPage, getPages, pageTree } = loader({
@@ -8,11 +10,19 @@ export const { getPage, getPages, pageTree } = loader({
   source: createMDXSource(map),
 });
 
-const blogLoader = loader({
+export const blogLoader = loader({
   baseUrl: '/blog',
   rootDir: 'blog',
-  source: createMDXSource(map),
+  source: createMDXSource(map, {
+    schema: {
+      frontmatter: defaultSchemas.frontmatter.extend({
+        author: z.string(),
+        date: z.string().date().or(z.date()).optional(),
+      }),
+    },
+  }),
 });
+
 
 export const {
   getPage: getBlogPage,
