44cdb239352cc784f2ada81aa2f2a3b8a1718790
Xiangzhuang Shen <datahonor@gmail.com>
2024-07-24 16:57:03 +0800

    format

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,12 +1,12 @@
 // app/providers.tsx
-"use client";
-import { RootProvider } from "fumadocs-ui/provider";
-import { PythonProvider } from "react-py";
+'use client';
+import { RootProvider } from 'fumadocs-ui/provider';
+import { PythonProvider } from 'react-py';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 
-import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 
 const inject = `
 const urlParams = new URLSearchParams(window.location.search);
@@ -17,28 +17,32 @@ if (typeof uwuParam === 'string') {
 }
 
 const item = localStorage.getItem('uwu')
-    
+
 if (item === 'true') {
     document.documentElement.classList.add("uwu")
-}    
+}
 `;
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <RootProvider>
-      <CacheProvider>
-        <ChakraProvider>
+    <CacheProvider>
+      <ChakraProvider>
+        <RootProvider
+          theme={{
+            enabled: false,
+          }}
+        >
           <PythonProvider>
-      <TooltipProvider>
-        <script
-          suppressHydrationWarning
-          dangerouslySetInnerHTML={{ __html: inject }}
-        />
-        {children}
-      </TooltipProvider>
+            <TooltipProvider>
+              <script
+                suppressHydrationWarning
+                dangerouslySetInnerHTML={{ __html: inject }}
+              />
+              {children}
+            </TooltipProvider>
           </PythonProvider>
-        </ChakraProvider>
-      </CacheProvider>
-    </RootProvider>
+        </RootProvider>
+      </ChakraProvider>
+    </CacheProvider>
   );
 }
