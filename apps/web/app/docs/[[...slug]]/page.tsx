eed581c37adcff8e46f1149dfc625f085c4d6a36
Mathew Shen <datahonor@gmail.com>
2024-07-27 16:19:38 +0800

    docs: add edit on github button (#33)

--- apps/web/app/docs/[[...slug]]/page.tsx
+++ apps/web/app/docs/[[...slug]]/page.tsx
@@ -2,8 +2,11 @@ import { getPage, getPages } from '@/app/source';
 import type { Metadata } from 'next';
 import { DocsPage, DocsBody } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
-import { getGithubLastEdit } from 'fumadocs-core/server'
+import { getGithubLastEdit } from 'fumadocs-core/server';
 import { resolve } from 'url';
+import { cn } from '@/app/utils/cn';
+import { buttonVariants } from '@/app/components/blog/button';
+import { Edit } from 'lucide-react';
 
 export default async function Page({
   params,
@@ -16,6 +19,25 @@ export default async function Page({
     notFound();
   }
 
+  const path = `apps/web/content/docs/${page.file.path}`;
+  const footer = (
+    <a
+      href={`https://github.com/ai-glimpse/website/blob/main/${path}`}
+      target="_blank"
+      rel="noreferrer noopener"
+      className={cn(
+        buttonVariants({
+          variant: 'secondary',
+          size: 'sm',
+          className: 'text-xs gap-1.5',
+        })
+      )}
+    >
+      <Edit className="size-3" />
+      Edit on Github
+    </a>
+  );
+
   const MDX = page.data.exports.default;
 
   const lastEditDate = await getGithubLastEdit({
@@ -23,14 +45,19 @@ export default async function Page({
     repo: 'website',
     // example: "content/docs/index.mdx"
     path: resolve('apps/web/content/docs/', page.file.path),
-    token: `Bearer ${process.env.GIT_TOKEN}`
+    token: `Bearer ${process.env.GIT_TOKEN}`,
   });
 
   return (
     <DocsPage
       {...(lastEditDate ? { lastUpdate: new Date(lastEditDate) } : {})}
       toc={page.data.exports.toc}
-      full={page.data.full}>
+      full={page.data.full}
+      tableOfContent={{
+        footer,
+      }}
+      tableOfContentPopover={{ footer }}
+    >
       <DocsBody>
         <h1>{page.data.title}</h1>
         <MDX />
