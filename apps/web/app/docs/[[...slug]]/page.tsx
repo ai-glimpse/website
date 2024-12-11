90f25bef5db6fe30449ea27a9359017fbc07868d
Xiangzhuang Shen <datahonor@gmail.com>
2024-12-11 20:41:24 +0800

    test pass

--- apps/web/app/docs/[[...slug]]/page.tsx
+++ apps/web/app/docs/[[...slug]]/page.tsx
@@ -7,6 +7,13 @@ import {
 } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { getGithubLastEdit } from 'fumadocs-core/server'
+import { resolve } from 'url';
+import Comments from '@/app/components/comments';
+import { cn } from '@/app/utils/cn';
+import { buttonVariants } from '@/app/components/blog/button';
+import { Edit } from 'lucide-react';
+
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -15,15 +22,52 @@ export default async function Page(props: {
   const page = source.getPage(params.slug);
   if (!page) notFound();
 
+  const path = `apps/web/content/docs/${page.file.path}`;
+  const footer = (
+    <a
+      href={`https://github.com/ai-glimpse/website/blob/main/${path}`}
+      target="_blank"
+      rel="noreferrer noopener"
+      className={cn(
+        buttonVariants({
+          variant: 'secondary',
+          size: 'sm',
+          className: 'text-xs gap-1.5',
+        })
+      )}
+    >
+      <Edit className="size-3" />
+      Edit on Github
+    </a>
+  );
+
+
   const MDX = page.data.body;
 
+  const lastEditDate = await getGithubLastEdit({
+    owner: 'ai-glimpse',
+    repo: 'website',
+    // example: "content/docs/index.mdx"
+    path: resolve('apps/web/content/docs/', page.file.path),
+    token: `Bearer ${process.env.GIT_TOKEN}`
+  });
+
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      {...(lastEditDate ? { lastUpdate: new Date(lastEditDate) } : {})}
+      toc={page.data.toc}
+      full={page.data.full}
+      tableOfContent={{
+        footer,
+      }}
+      tableOfContentPopover={{ footer }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
         <MDX components={{ ...defaultMdxComponents }} />
       </DocsBody>
+      <Comments />
     </DocsPage>
   );
 }
--- apps/web/lib/source.ts
+++ apps/web/lib/source.ts
@@ -1,9 +1,20 @@
 import { docs, meta, blog as blogPosts } from '@/.source';
 import { createMDXSource } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
+
 
 export const source = loader({
   baseUrl: '/docs',
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
   source: createMDXSource(docs, meta),
 });
 
