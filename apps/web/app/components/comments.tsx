e6a2d3ec84806db52584384db423f39b489c5c0a
Xiangzhuang Shen <datahonor@gmail.com>
2025-06-05 14:45:31 +0800

    fixed other issues

--- apps/web/app/components/comments.tsx
+++ apps/web/app/components/comments.tsx
@@ -3,20 +3,23 @@
 import Giscus from "@giscus/react";
 
 export default function Comments() {
+
   return (
-    <Giscus
-      id="comments"
-      repo="ai-glimpse/website"
-      repoId="R_kgDOLchO6A"
-      category="Announcements"
-      categoryId="DIC_kwDOLchO6M4CdxBU"
-      mapping="pathname"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme="preferred_color_scheme"
-      lang="en"
-      loading="lazy"
-    />
+    <div className="mt-8">
+      <Giscus
+        id="comments"
+        repo="ai-glimpse/website"
+        repoId="R_kgDOLchO6A"
+        category="Announcements"
+        categoryId="DIC_kwDOLchO6M4CdxBU"
+        mapping="pathname"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="preferred_color_scheme"
+        lang="en"
+        loading="lazy"
+      />
+    </div>
   );
 }
